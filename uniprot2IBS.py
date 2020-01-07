6aad112d924e1d8c16d1b2b9a751651803465fe7
Stella3055 <raven_uw@yahoo.com>
2020-01-07 13:13:29 +0800

    200107:finish the demo

--- uniprot2IBS.py
+++ uniprot2IBS.py
@@ -1,30 +1,121 @@
-class uniprot2IBS(object):
-    def __init__(self, pathToUniprotXml):
-        import configparser
-        self.inPath = pathToUniprotXml
+import jinja2
+import pandas as pd
+import xml.etree.ElementTree as et
+from collections import OrderedDict
 
+class uniprot2Info(object):
+    def __init__(self, pathToUniprotXml):
+        self.inPath = pathToUniprotXml
         self.xmlParse()
         return
 
     def xmlParse(self):
-        import xml.etree.ElementTree as et
-        from collections import OrderedDict
         parser = et.parse(self.inPath)
         root = parser.getroot()
-
         featureList = []
         for features in root.iter(tag="{http://uniprot.org/uniprot}feature"):
-            featureList.append([features.get("type"), features.get("description")])
-        print(featureList)
-        for i in featureList:
-            print(lambda i:"\t".join(i))
-        print("\n".join(map(lambda x:"\t".join(x), featureList)))
-            # if features.get("type") == "region of interest":
-            #     for subfeatures in features.iter(tag="{http://uniprot.org/uniprot}location"):
-            #         print(subfeatures.find("begin").get("position"))
-                # loc = features.find("location")
-                # print(loc.find("begin"))
+            posStart = "NA"
+            posEnd = "NA"
+            posLoci = "NA"
+            for childElem in features.getchildren():
+                if childElem.tag == "{http://uniprot.org/uniprot}location":
+                    for subChildElem in childElem.getchildren():
+                        # print(subChildElem.tag)
+                        if subChildElem.tag == "{http://uniprot.org/uniprot}begin":
+                            posStart = subChildElem.get("position")
+                        if subChildElem.tag == "{http://uniprot.org/uniprot}end":
+                            posEnd = subChildElem.get("position")
+                        if subChildElem.tag == "{http://uniprot.org/uniprot}position":
+                            posLoci = subChildElem.get("position")
+            featureList.append([
+                features.get("type"),
+                features.get("description", default = "NA"),
+                posStart,
+                posEnd,
+                posLoci
+                ])
+        header = "\t".join([
+            "feature_name",
+            "description",
+            "Start_Position",
+            "End_Position",
+            "Loci_Position"
+        ])
+        # print(header)
+        # for feature in featureList:
+        #     print("\t".join(feature))
+
+        with open(self.inPath+"info.tsv", "w") as output:
+            output.write(header+"\n")
+            for feature in featureList:
+                output.write("\t".join(feature)+"\n")
+        return
+
+
+class info2IbsProj(object):
+    def __init__(self, pathToInfo):
+        self.pathToInfo = pathToInfo
+        self.importInfo()
+        self.importTemplates()
+        self.writeIbsXml()
+        return
+
+    def importInfo(self):
+        self.df=pd.read_table(self.pathToInfo)
+        return
+
+    def importTemplates(self):
+        with open("./templates/chain.xml", "r") as chainTemplate:
+            self.chainTemplate = chainTemplate.read()
+        with open("./templates/domain.xml", "r") as domainTemplate:
+            self.domainTemplate = domainTemplate.read()
+        with open("./templates/site.xml", "r") as siteTemplate:
+            self.siteTemplate = siteTemplate.read()
+        return
+
+    def renderDomain(self, domainRow):
+        domain = {}
+        domain["id"] = "Domain"+str(domainRow.name)
+        domain["name"] = domainRow.description
+        domain["start"] = int(domainRow.Start_Position)
+        domain["end"] = int(domainRow.End_Position)
+        return jinja2.Template(self.domainTemplate).render(domain=domain)
+
+    def renderSite(self, siteRow):
+        site = {}
+        site["id"] = "Site"+str(siteRow.name)
+        site["name"] = str(int(siteRow.Loci_Position))
+        site["pos"] = int(siteRow.Loci_Position)
+        return jinja2.Template(self.siteTemplate).render(site=site)
+
+    def renderXml(self):
+        chain = {}
+        chainSeries = self.df[self.df.feature_name == "chain"]
+        # print(chainSeries.description[0])
+
+        chain["name"] = chainSeries.description[0]
+        chain["start"] = int(chainSeries.Start_Position[0])
+        chain["end"] = int(chainSeries.End_Position[0])
+        chain["components"] = []
+
+        compDf = self.df[self.df.feature_name != "chain"]
+        for index, row in compDf.iterrows():
+            if pd.isnull(row.Loci_Position):
+                chain["components"].append(self.renderDomain(row))
+            else:
+                chain["components"].append(self.renderSite(row))
+
+        # print(chain)
+        print(jinja2.Template(self.chainTemplate).render(chain=chain))
+        return jinja2.Template(self.chainTemplate).render(chain=chain)
+
+    def writeIbsXml(self):
+        with open(self.pathToInfo+".ibs.xml", "w") as output:
+            output.write(self.renderXml())
+        return
 
 if __name__ == "__main__":
     pathToUniprotXml = "./ESR1.xml"
-    uniprot2IBS_test = uniprot2IBS(pathToUniprotXml)
+    pathToInfo = "./ESR1.xmlinfo.tsv"
+    # uniprot2IBS_test = uniprot2Info(pathToUniprotXml)
+    info2IbsProj_test = info2IbsProj(pathToInfo)
